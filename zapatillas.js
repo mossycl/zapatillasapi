9c75a32e80cadf276931af578fb460f0ef338d03
mossycl <163337883+mossycl@users.noreply.github.com>
2024-05-31 20:55:48 -0400

    Update zapatillas.js

--- zapatillas.js
+++ zapatillas.js
@@ -5,7 +5,7 @@ const zapatillas = [{
         "price" : 162990,
         "desc" : "Vuelve a descubrir un clásico con los Jordan 4 Retro Bred Reimagined, que salieron a la venta el 17 de febrero de 2024. Estos sneakers son una nueva versión de los emblemáticos Jordan 4, y aportan un toque contemporáneo a un diseño atemporal. Este nuevo lanzamiento deja atrás el tradicional nubuck e incorpora una nueva versión de la parte superior con piel negra flexible. La serie Reimagined, conocida por dar un nuevo impulso a los Air Jordan clásicos con materiales innovadores, convierte a esta versión en una auténtica pieza de coleccionista.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "https://nikeclprod.vtexassets.com/arquivos/ids/959050-800-800?v=638425072655630000&width=800&height=800&aspect=true",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -15,7 +15,7 @@ const zapatillas = [{
         "price" : 88990,
         "desc" : "El fulgor vive en Nike Air Force 1 '07, el ícono del básquetbol que le da un toque fresco a las características más recordadas: colores audaces y la cantidad perfecta de destellos para que brilles.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "https://nikeclprod.vtexassets.com/arquivos/ids/870137-800-800?v=638333320778000000&width=800&height=800&aspect=true",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -25,7 +25,7 @@ const zapatillas = [{
         "price" : 104990,
         "desc" : "Ya sea que seas jugador de básquetbol, que hagas skateboarding o que simplemente te encante el calzado, el Nike Dunk Low ha sido un favorito entre los atletas durante décadas. El cuero sintético duradero te brinda una sensación clásica de los 80, y la suela de goma ofrece agarre y tracción.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "https://nikeclprod.vtexassets.com/arquivos/ids/941693-800-800?v=638405217880630000&width=800&height=800&aspect=true",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -35,7 +35,7 @@ const zapatillas = [{
         "price" : 61990,
         "desc" : "Con textiles resistentes y materiales repelentes al agua donde hacen falta, estas botas flexibles están hechas para la aventura. A tu pequeño explorador le gusta hacer las cosas por sí solo, así que diseñamos asas para que sea muy fácil poner estas botas. La suela de goma, con tracción inspirada en los neumáticos de los camiones tipo monster truck, les da el agarre que necesitan para parques infantiles, aceras y lugares fangosos. Ensuciarse nunca se vio tan bien.¡Salpica, salpica!",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "https://nikeclprod.vtexassets.com/arquivos/ids/885790-800-800?v=638356787603400000&width=800&height=800&aspect=true",
         "tag" : ["niño", "zapatillas"]
     },
     {
@@ -55,7 +55,7 @@ const zapatillas = [{
         "price" : 64990,
         "desc" : "La zapatilla Old Skool, la clásica zapatilla de skate de Vans es la primera en tener la icónica sidestripe en los lados, es una zapatilla de caña baja con cordón que tiene cuero de lona y gamuza resistente, punteras reforzadas para soportar el desgaste por el continuo uso, caña acolchada para dar soporte y flexibilidad y la clásica suela de goma vulcanizada de la marca.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "/assets/img/vans/vans-classic.jpg",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -65,7 +65,7 @@ const zapatillas = [{
         "price" : 74990,
         "desc" : "Conocida inicialmente como Vans #36, Old Skool debutó en 1977 con una nueva incorporación singular: un garabato aleatorio dibujado por su fundador, Paul Van Doren, que originalmente recibió el nombre 'raya de jazz'. Actualmente, la famosa sidestripe de Vans se transformó en el sello distintivo e inmediatamente reconocible de la marca Vans.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "assets/img/vans/vans-bolt-twotone.jpg",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -75,7 +75,7 @@ const zapatillas = [{
         "price" : 74990,
         "desc" : "La nueva actualización de las adidas Adizero Boston 12 mantienen la esencia de esa legendaria saga de zapatillas mixtas de la marca alemana para adaptarse a los entrenamientos de calidad más exigentes, incluso a las competiciones con las mejores garantías de éxito sobre el asfalto. Tecnología puntera de adidas evolucionada para ofrecer transiciones más suaves, pero sin tocar esos apartados tan relevantes como amortiguación, dinamismo y eficiencia en carrera.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "/assets/img/adidas/adizero-boston.jpg",
         "tag" : ["unisex", "zapatillas"]
     },
     {
@@ -85,7 +85,7 @@ const zapatillas = [{
         "price" : 74990,
         "desc" : "Zapatillas para combinar con tu ropa favorita. Estilo Unisex, para elegir tu talla por favor asegúrate viendo los centímetros en nuestra guía de tallas.",
         "sizes" : [39,40,41,43,44,45],
-        "img" : "",
+        "img" : "/assets/img/dc/dc-court-graffic.jpg",
         "tag" : ["unisex", "zapatillas"]
     },
 ];
